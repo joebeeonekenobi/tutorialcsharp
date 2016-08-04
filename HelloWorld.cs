8d7eb106e453ec8e20176900fbcb6e74249bff4d
joebeeonekenobi <joe.butler@warwickgrad.net>
2016-08-04 11:53:11 +0100

    First Change
    First Change

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -4,6 +4,6 @@ public class HelloWorld{
 
 	public static void Main(string[] args){
 
-		Console.WriteLine("Hello World");
+		Console.WriteLine("Hello World, this is me teaching myself c# - and github as well!!!");
 	}
 }
