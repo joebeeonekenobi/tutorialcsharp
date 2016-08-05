6203006557e8f713dc56516d6bd5c5ff832d2196
joebeeonekenobi <joe.butler@warwickgrad.net>
2016-08-05 16:49:18 +0100

    Attributes Added

--- pointExample.cs
+++ pointExample.cs
@@ -1,4 +1,14 @@
+#define DEBUG
+
+
 using System;
+/*
+	by 'using' System, anything we reference that is not found 
+	such as Console, when using Console.WriteLine, searches System,
+	and other namespaces that we are 'using' to match.
+*/
+using System.Diagnostics;
+//This is required for 'Conditional[]' attribute
 
 //Interface Example
 public interface DisplaysData{
@@ -6,21 +16,30 @@ public interface DisplaysData{
 	string getDataString();
 }
 
-//Operator Overloading Example
 public class Point : DisplaysData{
 
 	public int x;
 	public int y;
 
 	public Point(int x, int y){
+
 		this.x = x;
 		this.y = y;
+		logCreation();
 	}
 
+	//Operator Overloading Example
 	public static Point operator+(Point a, Point b){
 
 		Point point = new Point(a.x + b.x, a.y + b.y);
 		return point;
+	}
+
+	//This function will only be called when DEBUG is defined;
+	[Conditional("DEBUG")]
+	public void logCreation(){
+
+		Console.WriteLine("New Point Created: ("+ this.x +", "+ this.y +")");
 	}
 
 	//Obeys Interface
@@ -29,11 +48,23 @@ public class Point : DisplaysData{
 		return this.x + ", " + this.y;
 	}
 
+	[Obsolete("Do not use this function at all", true)]
+	public string getString(){
+
+		Console.WriteLine("This function is deprecated.");
+		return "";
+	}
+
 	public static void Main(string[] args){
 
 		Point pointA = new Point(5, 10);
 		Point pointB = new Point(1, 2);
 		Point pointC = pointA + pointB;
+
+		/*
+			This call will raise an error at compiletime
+			//Console.WriteLine(pointC.getString());
+		*/
 
 		Console.WriteLine(pointC.getDataString());
 	}
