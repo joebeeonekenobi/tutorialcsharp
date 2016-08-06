e5b20294b2d617bab3dc81d094baf7885c65eaf7
joebeeonekenobi <joe.butler@warwickgrad.net>
2016-08-06 19:03:00 +0100

    Attribute reflection understood.

--- pointExample.cs
+++ pointExample.cs
@@ -1,4 +1,5 @@
 #define DEBUG
+//Preprocessor symbol which cannot take a value, and does not conflict with any in-program variables names.
 
 
 using System;
@@ -9,6 +10,8 @@ using System;
 */
 using System.Diagnostics;
 //This is required for 'Conditional[]' attribute
+using System.Collections;
+//For a Stack
 
 //Interface Example
 public interface DisplaysData{
@@ -16,6 +19,29 @@ public interface DisplaysData{
 	string getDataString();
 }
 
+//Attribute Example
+
+//This tag restricts how the attribute can be used, I have allowed all components.
+//I have also redundantly set the AllowMultiple field to false (default false), so that it can only be applied to each method at max once.
+[AttributeUsage(AttributeTargets.All, AllowMultiple = false)]
+public class OverloadTag : System.Attribute{
+
+	/*
+		An attribute is essentially just an object like any other, that implements
+		the System.attribute interface.
+	*/
+
+	public string operatorName;
+	public int overloadNumber;
+
+	public OverloadTag(string name, int number){
+
+		this.operatorName = name;
+		this.overloadNumber = number;
+	}
+}
+
+[OverloadTag("plus", 0)]
 public class Point : DisplaysData{
 
 	public int x;
@@ -33,6 +59,26 @@ public class Point : DisplaysData{
 
 		Point point = new Point(a.x + b.x, a.y + b.y);
 		return point;
+	}
+
+//!Currently broken
+	public static object[] getTags(){
+
+		//Get the attribute info for Point
+		System.Reflection.MemberInfo info = typeof(Point);
+
+		//Create a store for the attributes
+		Stack tags = new Stack();
+
+		//There may be many attributes for Point
+		foreach(var attribute in info.GetCustomAttributes(true)){
+
+			//Push them into the store
+			tags.Push(attribute);
+		}
+
+		//Return an array and not a stack
+		return tags.ToArray();
 	}
 
 	//This function will only be called when DEBUG is defined;
@@ -57,12 +103,27 @@ public class Point : DisplaysData{
 
 	public static void Main(string[] args){
 
+		/*
+			Preprocessor Code
+		*/
+		#if(DEBUG)
+			Console.WriteLine("Debugging is enabled.");
+		#endif
+		#if(TRACE)
+			Console.WriteLine("Tracing is enabled.");
+		#endif
+
 		Point pointA = new Point(5, 10);
 		Point pointB = new Point(1, 2);
 		Point pointC = pointA + pointB;
 
+		//Can now get the attribute tags in program
+		object[] tags = Point.getTags();
+		//The first tag is the OverloadTag.
+		Console.WriteLine(tags[0]);
+
 		/*
-			This call will raise an error at compiletime
+			This call will raise an error at compiletime as the method is marked as obsolete
 			//Console.WriteLine(pointC.getString());
 		*/
 
